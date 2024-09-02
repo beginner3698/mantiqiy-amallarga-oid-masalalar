e3fcb3854e70773246b33e013f9ed2162f76d92e
Akrombek <gaybulloyevakrom964@gmail.com>
2024-09-02 12:03:11 +0500

    boolean9

---
+++ boolean9/script.js
@@ -0,0 +1,22 @@
+function chiqar() {
+    let A = prompt()
+    let B = prompt()
+
+    let bolish1 = A % 2
+    let bolish2 = B % 2
+
+    if (bolish1 == 0 && bolish2 > 0) {
+        console.log("Sonlarning bittasi toq");
+    }
+
+    else if (bolish1 > 0 && bolish2 == 0) {
+        console.log("Sonlarning bittasi toq");
+    }
+
+    else {
+        console.log("Ikkala son ham juft ");
+    }
+}
+
+
+chiqar()
