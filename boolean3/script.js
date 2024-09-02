551c7c51c6c8c0a2ef0dcc6bf107500753018195
Akrombek <gaybulloyevakrom964@gmail.com>
2024-09-02 11:57:16 +0500

    boolean3

---
+++ boolean3/script.js
@@ -0,0 +1,16 @@
+function chiqar() {
+    let A = prompt()
+
+    let bolish = A % 2
+
+    if (bolish == 0) {
+        console.log(A + " - juft son");
+    }
+
+    else {
+        console.log(A + " - toq son");
+    }
+}
+
+
+chiqar()
