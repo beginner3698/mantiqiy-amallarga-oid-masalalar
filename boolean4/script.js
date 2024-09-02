0d911898242b323796ddc3d9c72f448976597911
Akrombek <gaybulloyevakrom964@gmail.com>
2024-09-02 11:58:30 +0500

    boolean4

---
+++ boolean4/script.js
@@ -0,0 +1,19 @@
+function chiqar() {
+    let A = prompt()
+    let B = prompt()
+    
+    if (A > 2) {
+        console.log("True");
+    }
+
+    else if (B <= 3) {
+        console.log("True");
+    }
+
+    else {
+        console.log("False");
+    }
+}
+
+
+chiqar()
