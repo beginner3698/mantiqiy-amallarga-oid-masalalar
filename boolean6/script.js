b7890d44c955aa1dc5e6860aa841b0a74d606a5a
Akrombek <gaybulloyevakrom964@gmail.com>
2024-09-02 12:00:10 +0500

    boolean6

---
+++ boolean6/script.js
@@ -0,0 +1,16 @@
+function chiqar() {
+    let A = prompt()
+    let B = prompt()
+    let C = prompt()
+
+    if (A <= B && B <= C && A <= C) {
+        console.log("True");
+    }
+
+    else {
+        console.log("False");
+    }
+}
+
+
+chiqar()
