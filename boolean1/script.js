0361e7ec5380c70129b903bafd54088e6ccbaeea
Akrombek <gaybulloyevakrom964@gmail.com>
2024-09-02 11:54:41 +0500

    boolean1

---
+++ boolean1/script.js
@@ -0,0 +1,14 @@
+function chiqar() {
+    let A = prompt()
+
+    if (A >= 0) {
+        console.log(A + " - musbat son");
+    }
+
+    else {
+        console.log(A + " - manfiy son");
+    }
+}
+
+
+chiqar()
